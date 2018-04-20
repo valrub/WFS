582f9083e5dd59fe1e8e3bbd73c0750d2e422ea8
Valentin <valrub@gmail.com>
2018-04-20 18:28:35 +0300

    VK new

---
+++ VK/VK_Target/vk_collect_about_info.js
@@ -0,0 +1,431 @@
+function main(re, ie, oe, executor) {
+    //=====================================================================
+    //Initialize Global Settings
+    setGlobalLogger(re, ie, oe, executor);
+
+
+
+
+	executionContext = {
+		globalLogExtracted: false, //change to false before release;
+		globalWPXP: xpaths.VK_Target
+	};
+
+	var _extract = new Extract(executionContext);
+	var _process = new Process();
+
+    //------------- VAL : check that VA is really loged in ---------------
+//setTimeout(function(){
+    var vProfileName = _extract.GetText({
+                xpathName: ".//div[@class='top_profile_name']",
+        		mandatory: "0"
+			},
+			"ProfileName"
+		).Value;
+        Logger.production('Try to login. vProfileName = ' + vProfileName)
+        
+        if(!(vProfileName === null)){
+    		Logger.production("The agent logged in successfully!!!");
+			
+		}else{
+    	    Logger.failure("The agent did not log in! The flow will fail!!!", "400020");
+            executor.ready();
+		}
+//}, 3000);
+// --------------------------------------------------------------------
+
+
+
+
+
+	//=====================================================================
+	// GLOBAL VARIABLES
+
+	var collectedAuthors = [];
+	var theTargetID = "";
+	var cntItems = 0;
+	var totalAuthors = 0;
+	var _tmp;
+
+	//=====================================================================
+	var CAName = "VK About Collect";
+	Logger.production(CAName + " Start Time: " + new Date());
+
+	//===============================================================================
+	// -------------------  Here extract logic begins  ------------------------------
+
+	_process.Run(collectTargetEntity, true, {
+		pMarker: '1',
+		functionName: 'collectTargetEntity'
+	});
+
+	Logger.production('------------- 1 -----------------');
+	_process.Run(collectKeyValues, false, {
+		pMarker: '2',
+		functionName: 'collectKeyValues'
+	});
+
+	Logger.production('------------- 2 -----------------');
+	_process.Run(collectAboutInfo, false, {
+		pMarker: '3',
+		functionName: 'collectAboutInfo'
+	});
+
+	Logger.production('------------- 3 -----------------');
+
+	// ----------------------------- HANDLERS  --------------------------------------
+	function collectTargetEntity(pMarker, pContext) {
+
+		cntItems = 0;
+		var _res = {
+			totalCollected: 0,
+			returnCode: ""
+		};
+
+		try {
+              
+            //Check for hidden profile
+            var vHiddenProfile = _extract.GetText({
+					xpathName: "vHiddenProfile",
+					mandatory: "0"
+				},
+				pMarker + "0"
+			).Value;
+            
+            if (vHiddenProfile === "Profile hidden") {
+                Logger.failure("The target profile is hidden. It can only be accessed with a logged in agent. Check the agent.", "400020");
+            }
+            
+            //Check for blocked profile
+            var vBlockedProfile = _extract.GetText({
+                    xpathName: "vBlockedProfile",
+					mandatory: "0"
+				},
+				pMarker + "0"
+			).Value;
+            
+            if (vBlockedProfile) {
+                Logger.failure("The target profile is blocked.");
+            }          
+            
+			var vAccountID = _extract.GetText({
+					xpathName: "tTargetName",
+					mandatory: "1"
+				},
+				pMarker + "-1"
+			).Value;
+
+			var vFullName = _extract.GetText({
+					xpathName: "tTargetName",
+					mandatory: "1"
+				},
+				pMarker + "-2"
+			).Value;
+
+			var vImageURL = _extract.GetAttribute({
+					xpathName: "aAvatarURL",
+					attributeName: "src",
+					mandatory: "0"
+				},
+				pMarker + "-3"
+			).Value;
+
+			var aFriendsPageUrl = _extract.GetAttribute({
+					xpathName: "aFriendsPageUrl",
+					attributeName: "href",
+					mandatory: "0"
+				},
+				pMarker + "-0"
+			);
+
+			var aPhotosPageUrl = _extract.GetAttribute({
+					xpathName: "aPhotosPageUrl",
+					attributeName: "href",
+					mandatory: "0"
+				},
+				pMarker + "-01"
+			);
+
+			if (aFriendsPageUrl.returnCode === "200") {
+				re.gender = aFriendsPageUrl.Value; // used to load the friends page
+				Logger.production("The target profile has a friends list: " + re.gender);
+			} else {
+				re.placeholder1 = false;
+				Logger.production("The target profile has no friends list. " + aFriendsPageUrl.returnCode);
+			}
+
+			if (aPhotosPageUrl.returnCode === "200") {
+				re.placeholder2 = aPhotosPageUrl.Value; // used to load the photos page
+				Logger.production("The target profile has a friends list: " + re.placeholder2);
+			} else {
+				Logger.production("The target profile has no photos. " + aPhotosPageUrl.returnCode);
+			}
+
+			// Save target entity -------------------------------------------
+
+            //Logger.production('VAL-1');
+
+			var target = {};
+
+			target.itemType = "4"; // Web Entity
+			target.type = "1"; // Person
+			target.activityType = "1"; // Social Network
+			target.title = vFullName;
+			target.imageUrl = vImageURL;
+            target.url = window.location.href; //Current page
+            
+            
+            //Logger.production('VAL-2');
+            var preID = window.location.href.toString().match(/https:\/\/vk\.com\/(.+)/)[1];
+			
+            var rightID; 
+
+
+            Logger.production('VAL777-1: Try to login. rightID = ' + rightID)
+            
+
+
+
+			try //Id could be in format id123456 
+				//tHEN - let's clean out "id" in case it exists because in other places it will appear without it (only digits)
+			{
+				rightID = preID.match(/id(.+)/)[1];
+				Logger.production('Account ID is id12345 style = ' + rightID);
+
+			} //or it could be just a name like mike.smith or zubbrr123
+			catch(e)
+			{
+				var isNotGroup = document.evaluate(".//div[@class='counts_module']/a[contains(@href, '/friends?id=')]", document, null, 9, null).singleNodeValue;
+				if(!isNotGroup)
+					{
+						Logger.failure('VK Group - not supported.');
+					}
+					else
+					{
+						rightID = document.evaluate(".//div[@class='counts_module']/a[contains(@href, '/friends?id=')]", document, null, 9, null).singleNodeValue.getAttribute("href").match(/(\/friends\?id=)(\d+)(&section=all)/)[2];
+						Logger.production('Account ID is NOT id12345 style = ' + preID + " so ID was fpound -->" + rightID);
+					}
+
+
+				
+			}
+
+
+            //Logger.production('VAL-3');
+
+
+
+
+
+            //target.externalId = hashCode(window.location.href.toString().match(/https:\/\/vk\.com\/(.+)/)[1]);
+            target.externalId = hashCode(rightID);
+
+            //target.description = rightID; //VAL
+
+			theTargetID = target.externalId;
+			re.targetId = theTargetID;
+			cntItems++;
+
+			if (!collectedAuthors.hasOwnProperty(target.externalId)) {
+				collectedAuthors[target.externalId] = true;
+
+				totalAuthors++;
+				addImage(target);
+				cntItems++;
+			}
+
+
+            //Logger.production('VAL-4');
+			// Save IsMonitored entity -------------------------------------------
+			var TargetIsMonitoredEntity = {};
+
+			TargetIsMonitoredEntity.itemType = "18";
+			TargetIsMonitoredEntity.parent_externalId = theTargetID.toString();
+			TargetIsMonitoredEntity.parentObjectType = "4";
+
+			addEntity(TargetIsMonitoredEntity);
+
+			Logger.production("Is Monitored - added for " + theTargetID);
+
+			//--------------------------------------------------------------------
+
+			_res.totalCollected = cntItems;
+			_res.returnCode = "200";
+			return _res;
+		} catch (e) {
+			_res.returnCode = "504 " + e.message;
+			return _res;
+		}
+	}
+	//....................................................
+
+	function getInfo(thisNode, parentExternalId) {
+		for (var key in aboutInfoStructure) {
+			if (aboutInfoStructure.hasOwnProperty(key)) {
+				if (aboutInfoStructure[key].label === thisNode.textContent) {
+					var vRowValue = _extract.GetText({
+							xpathName: ".//*[contains(@class,'label fl_l') and contains(text(),'" + aboutInfoStructure[key].label + "')]/following-sibling::div",
+							mandatory: "0"
+						},
+						"RowValue"
+					).Value;
+
+					var entity = {
+						itemType: aboutInfoStructure[key].entityItemType,
+						type: aboutInfoStructure[key].entityType,
+						body: vRowValue,
+						title: aboutInfoStructure[key].label,
+						url: "no url given",
+						description: vRowValue,
+                        activityType: "2"
+					};
+
+					if (aboutInfoStructure[key].entityItemType !== "4") {
+						entity.parent_externalId = parentExternalId;
+						entity.parentObjectType = "4";
+
+						if (aboutInfoStructure[key].entityItemType === "15") {
+							entity.writer_externalId = parentExternalId;
+						}
+					} else {
+
+						entity.externalId = vRowValue + parentExternalId;
+						if (aboutInfoStructure[key].relationType !== "") {
+							var rel = {};
+							rel.parent_externalId = parentExternalId;
+							rel.parentObjectType = "4";
+							rel.activityType = "2";
+							rel.description = aboutInfoStructure[key].label;
+							rel.title = aboutInfoStructure[key].label;
+							rel.type = aboutInfoStructure[key].relationType;
+							rel.itemType = "12";
+							rel.sideB_externalId = entity.externalId;
+							rel.sideB_ObjectType = "4";
+							addEntity(rel);
+						}
+					}
+					addEntity(entity);
+				}
+			} else {
+				Logger.production("New about info label added by VK: " + thisNode.textContent);
+			}
+		}
+	}
+
+	function collectAboutInfo(pMarker, pContext) {
+		try {
+
+			cntItems = 0;
+			var _res = {
+				totalCollected: 0,
+				returnCode: ""
+			};
+
+			_tmp = _extract.GetCollection({
+					xpathName: "vInfoRowLabel",
+					mandatory: "0"
+				},
+				pMarker
+			);
+
+			if (_tmp.returnCode === "200") {
+				var iterator = _tmp.Value;
+				var thisNode = iterator.iterateNext();
+
+				while (thisNode) {
+					cntItems++;
+					getInfo(thisNode, theTargetID);
+					thisNode = iterator.iterateNext();
+				}
+				_res.totalCollected = cntItems;
+				_res.returnCode = "200";
+			} else { //No info found
+				_res.totalCollected = 0;
+				_res.returnCode = "204";
+			}
+
+		} catch (e) {
+			Logger.error("collectAboutInfo('collectAboutInfo', '5' - DID NOT WORK - " + e.message);
+			_res.totalCollected = cntItems;
+			_res.returnCode = "504 " + e.message;
+		}
+		return _res;
+	}
+
+	function collectKeyValues(pMarker, pContext) {
+		try {
+			cntItems = 0;
+			var _res = {
+				totalCollected: 0,
+				returnCode: ""
+			};
+			var counters = _extract.GetCollection({
+					xpathName: "vKeyValueCounters",
+					mandatory: "0"
+				},
+				pMarker
+			);
+
+			if (counters.returnCode === "200") {
+				var i = counters.Value;
+				var thisNode = i.iterateNext();
+
+				while (thisNode) {
+					cntItems++;
+					var vCount = _extract.GetText({
+							context: thisNode,
+							xpathName: "./*[contains(@class, 'count')]",
+							mandatory: "0"
+						},
+						"vPostText"
+					).Value;
+
+					var vLabel = _extract.GetText({
+							context: thisNode,
+							xpathName: "./*[contains(@class, 'label')]",
+							mandatory: "0"
+						},
+						"vPostText"
+					).Value;
+
+					var keyValue = {};
+					keyValue.itemType = "24";
+					keyValue.body = vCount;
+					keyValue.title = vLabel;
+					keyValue.description = vLabel;
+					keyValue.parent_externalId = theTargetID;
+					keyValue.parentObjectType = "4";
+
+					thisNode = i.iterateNext();
+					addEntity(keyValue);
+				}
+                _res.totalCollected = cntItems;
+    			_res.returnCode = "200";
+			} else { //No info found
+				_res.totalCollected = 0;
+				_res.returnCode = "204";
+			}
+
+		} catch (e) {
+			Logger.error("collectKeyValues('collectKeyValues', '5' - DID NOT WORK - " + e.message);
+			_res.totalCollected = cntItems;
+			_res.returnCode = "504 " + e.message;
+		}
+		return _res;
+	}
+
+	function hashCode(input) {
+		var hash = 0;
+		if (input.length === 0) return hash;
+		for (var i = 0; i < input.length; i++) {
+			var character = input.charCodeAt(i);
+			hash = ((hash << 5) - hash) + character;
+			hash = hash & hash; // Convert to 32bit integer
+		}
+		return hash.toString();
+	}
+
+	Logger.production(CAName + " End Time: " + new Date());
+	finalize();
+
+}
---
+++ VK/VK_Target/vk_collect_photos.js
@@ -0,0 +1,129 @@
+function main(re, ie, oe, executor) {
+    if (re.executionContext.indexOf("photos") === -1) {
+		executor.ready();
+	}
+	//=====================================================================
+	//Initialize Global Settings
+	setGlobalLogger(re, ie, oe, executor, 4);
+
+	executionContext = {
+		globalLogExtracted: false, //change to false before release;
+		globalWPXP: xpaths.VK_Target
+	};
+
+	var _extract = new Extract(executionContext);
+	var _process = new Process();
+
+	//=====================================================================
+	// GLOBAL VARIABLES
+
+	var collectedAuthors = [];
+	var theTargetID = re.targetId;
+	var cntItems = 0;
+	var totalAuthors = 0;
+	var _tmp;
+
+	//=====================================================================
+	var CAName = "VK Photos Collect";
+	Logger.production(CAName + " Start Time: " + new Date());
+
+	//===============================================================================
+	// -------------------  Here extract logic begins  ------------------------------
+
+	_process.Run(collectPhotos, false, {
+		pMarker: '6',
+		functionName: 'collectPhotos'
+	});
+
+	function hashCode(input) {
+		var hash = 0;
+		if (input.length === 0) return hash;
+		for (var i = 0; i < input.length; i++) {
+			var character = input.charCodeAt(i);
+			hash = ((hash << 5) - hash) + character;
+			hash = hash & hash; // Convert to 32bit integer
+		}
+		return hash.toString();
+	}
+
+	function collectPhotos(pMarker, pContext) {
+		try {
+			cntItems = 0;
+			var _res = {
+				totalCollected: 0,
+				returnCode: ""
+			};
+
+			var ttmp = _extract.GetCollection({
+					xpathName: "aPhotos",
+					mandatory: "0"
+				},
+				pMarker
+			);
+
+			if (ttmp.returnCode === "200") {
+				var iterator = ttmp.Value;
+				var thisNode = iterator.iterateNext();
+				var i = 0;
+
+				while (thisNode && parseInt(re.placeholder4) > i) {
+
+					//thisNode.click();
+					
+					var date = "";
+					var hrefDoc = document.evaluate(".//a", thisNode, null, 9, null).singleNodeValue;
+					var xhr = new XMLHttpRequest();
+					xhr.ajaxUrl = hrefDoc.href;
+					Logger.debug("currentImage: " + hrefDoc.href);
+					xhr.open('GET', xhr.ajaxUrl, false);
+					xhr.send();
+					if (xhr.status === 200) {
+						var urlHref = hrefDoc.href.split('?');
+						var urlSplit = urlHref[0].match(/([0-9]+_+[0-9]+)/)[0];
+						var parser = new DOMParser();
+						var html = parser.parseFromString(xhr.responseText, "text/html");
+						var first = xhr.responseText.indexOf('"id":"' + urlSplit + '"');
+						var second = xhr.responseText.substring(first, xhr.responseText.length);
+						var third = second.indexOf('"tags"');
+						//console.log(third);
+						var final = second.substring(0, third);
+						//console.log(final);
+						if (final.match(/(\d+\s+\w+\s+\w+\s+\d+\W+\d+\s+\w+)|(\d+\s+\w+\s+\d+)/gi)) {
+							var matches = final.match(/\d+\s+\w+\s+\w+\s+\d+\W+\d+\s+\w+|\d+\s+\w+\s+\d+/gi);
+							date = matches[matches.length-1];
+						} 
+						
+					}
+
+					i++;
+					var img = {};
+					img.parent_externalId = theTargetID;
+					img.parentObjectType = "4";
+					img.writer_externalId = theTargetID;
+					img.writeDate = date;
+					img.itemType = "5";
+					img.imageUrl = thisNode.getAttribute("style").match(/(https.+.jpg)/g)[0];
+					img.url = hrefDoc.href;
+					img.externalId = "photo_id_" + hashCode(img.imageUrl);
+					img.activityType = "1";
+
+					thisNode = iterator.iterateNext();
+					cntItems++;
+					addImage(img);
+				}
+				_res.totalCollected = cntItems;
+				_res.returnCode = "200";
+			} else { //No info found
+				_res.totalCollected = 0;
+				_res.returnCode = "204";
+			}
+		} catch (e) {
+			Logger.error("collectPhotos('collectPhotos', '6' - DID NOT WORK - " + e.message);
+			_res.totalCollected = cntItems;
+			_res.returnCode = "504 " + e.message;
+		}
+		return _res;
+	}
+	Logger.production(CAName + " End Time: " + new Date());
+	finalize();
+}
